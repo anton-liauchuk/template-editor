974f3050caa2ffa48b5dd9ffecfed04e94dbb9b0
Anton Liauchuk <anton93lev@gmail.com>
2018-10-31 06:45:35 +0300

    add checkboxes for configuring position table columns

--- src/main/java/com/codenotfound/primefaces/bean/PositionTableEditor.java
+++ src/main/java/com/codenotfound/primefaces/bean/PositionTableEditor.java
@@ -1,9 +1,15 @@
 package com.codenotfound.primefaces.bean;
 
+import com.codenotfound.primefaces.ColumnType;
 import com.codenotfound.primefaces.PositionTable;
+import com.codenotfound.primefaces.PositionTableColumnConfig;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.model.SelectItem;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @ManagedBean
@@ -11,12 +17,37 @@ import javax.faces.bean.ViewScoped;
 public class PositionTableEditor {
 
     private PositionTable positionTable;
+    private List<SelectItem> configs;
+    private List<ColumnType> selectedColumns;
 
     public void reset(PositionTable positionTable) {
         this.positionTable = positionTable;
+        configs = new ArrayList<>();
+        positionTable.getColumnConfigs().forEach(columnConfig -> configs.add(new SelectItem(columnConfig.getColumnType())));
+        selectedColumns = positionTable.getColumnConfigs()
+                .stream()
+                .filter(PositionTableColumnConfig::getEnabled)
+                .map(PositionTableColumnConfig::getColumnType)
+                .collect(Collectors.toList());
+    }
+
+    public void save() {
+        positionTable.getColumnConfigs().forEach(config -> config.setEnabled(selectedColumns.contains(config.getColumnType())));
     }
 
     public PositionTable getPositionTable() {
         return positionTable;
+    }
+
+    public List<SelectItem> getConfigs() {
+        return configs;
+    }
+
+    public List<ColumnType> getSelectedColumns() {
+        return selectedColumns;
+    }
+
+    public void setSelectedColumns(List<ColumnType> selectedColumns) {
+        this.selectedColumns = selectedColumns;
     }
 }

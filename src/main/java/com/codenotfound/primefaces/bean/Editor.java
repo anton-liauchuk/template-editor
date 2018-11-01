bd1c9b1272bc3bd1643fbac6a3a52016358f8390
Anton Liauchuk <anton93lev@gmail.com>
2018-11-01 06:39:49 +0300

    add pdf generation functionality

--- src/main/java/com/codenotfound/primefaces/bean/Editor.java
+++ src/main/java/com/codenotfound/primefaces/bean/Editor.java
@@ -5,16 +5,22 @@ import com.codenotfound.primefaces.DraggableComponent;
 import com.codenotfound.primefaces.EditableElement;
 import com.codenotfound.primefaces.Template;
 import com.codenotfound.primefaces.converter.EditableElementConverter;
+import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletResponse;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
+import java.io.OutputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -64,6 +70,31 @@ public class Editor {
     }
 
   }
+
+    public void createPDF() {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+        String servername = externalContext.getRequestServerName();
+        String port = String.valueOf(externalContext.getRequestServerPort());
+        String appname = externalContext.getRequestContextPath();
+        String protocol = externalContext.getRequestScheme();
+        String url = protocol + "://" + servername + ":" + port + appname + "/pdfTemplate.xhtml";
+        try {
+            ITextRenderer renderer = new ITextRenderer();
+            renderer.setDocument(new URL(url).toString());
+            renderer.layout();
+            HttpServletResponse response = (HttpServletResponse) externalContext.getResponse();
+            response.reset();
+            response.setContentType("application/pdf");
+            response.setHeader("Content-Disposition", "inline; filename=\"" + "pdfTemplate.pdf" + "\"");
+            OutputStream browserStream = response.getOutputStream();
+            renderer.createPDF(browserStream);
+
+        } catch (Exception ex) {
+            System.out.println("Exception during PDF Generation " + ex.getMessage());
+        }
+        facesContext.responseComplete();
+    }
 
   public List<EditableElement> getEditableElements() {
     return editableElements;
